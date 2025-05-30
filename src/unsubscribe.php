1e2642ffde1bef50c5a1256de6b6654c1e5a1416
Adityapushp <adityapushp.123@gmail.com>
2025-05-30 13:17:28 +0530

    unsubscribe.php

---
+++ src/unsubscribe.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Unsubscribe</title>
+</head>
+<body>
+&nbsp;
+&nbsp;
+
+<h1>Unsubscribe</h1>
+&nbsp;
+&nbsp;
+
+<form method="POST" action="unsubscribe.php">
+    <input type="email" name="unsubscribe_email" required>
+    <button id="submit-unsubscribe">Unsubscribe</button>
+</form>
+&nbsp;
+&nbsp;
+
+<form method="POST" action="unsubscribe.php">
+    <input type="text" name="verification_code" maxlength="6" required>
+    <button id="submit-verification">Verify</button>
+</form>
+&nbsp;
+&nbsp;
+
+</body>
+</html>
