0404e69346333333cc7a91bdcb7887bf111c2af2
Adityapushp <adityapushp.123@gmail.com>
2025-06-10 10:57:53 +0530

    setup_cron.sh

--- src/setup_cron.sh
+++ src/setup_cron.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-&nbsp;
-&nbsp;
 
-# This script sets up a cron job to run cron.php every 24 hours
-(crontab -l 2>/dev/null; echo "0 * * * * /usr/bin/php /path/to/your/src/cron.php") | crontab -
-echo "Cron job set to run cron.php every 24 hours."
+CRON_JOB="0 0 * * * /usr/bin/php /path/to/your/repo/src/cron.php"
+
+(crontab -l; echo "$CRON_JOB") | crontab -
+echo "Cron job has been set up to run cron.php every 24 hours."
